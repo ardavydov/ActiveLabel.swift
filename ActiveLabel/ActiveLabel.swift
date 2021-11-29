f35c7b1fb17d4503a0612a997fbb8babd0d378fd
Artem Davydov <yozzeebeatz@gmail.com>
2021-11-29 15:46:35 +0500

    Fix: switch default warning.

--- ActiveLabel/ActiveLabel.swift
+++ ActiveLabel/ActiveLabel.swift
@@ -236,7 +236,7 @@ typealias ElementTuple = (range: NSRange, element: ActiveElement, type: ActiveTy
             selectedElement = nil
         case .stationary:
             break
-        @unknown default:
+        default:
             break
         }
         
